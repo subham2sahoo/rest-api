7364d6f04b9b204131d5609eac7c8b755a98afc0
subham <suvm1sahoo@gmail.com>
2023-11-19 11:46:27 +0530

    firstCommit

--- src/main/java/com/sahoo/webservices/demoapi/helloworld/HelloWorld.java
+++ src/main/java/com/sahoo/webservices/demoapi/helloworld/HelloWorld.java
@@ -1,4 +1,4 @@
-package com.sahoo.webservices.demoapi;
+package com.sahoo.webservices.demoapi.helloworld;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
